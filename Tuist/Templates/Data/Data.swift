393678b652e77d11cd818edea25cbc9a8eda72c6
ji-yeon224 <69784492+ji-yeon224@users.noreply.github.com>
2025-06-02 14:03:51 +0900

    chore: Data Template 구현

---
+++ Tuist/Templates/Data/Data.swift
@@ -0,0 +1,51 @@
+//
+//  Data.swift
+//  Templates
+//
+//  Created by Jiyeon on 6/2/25.
+//
+
+import Foundation
+import ProjectDescription
+
+fileprivate let name: Template.Attribute = .required("feature")
+fileprivate let author: Template.Attribute = .required("author")
+
+/// 파일 경로
+fileprivate let DataPath: String = "Projects/Data/\(name)/"
+
+
+let dataTemplate = Template(
+  description: "Data Project Template",
+  attributes: [
+    name,
+    author
+  ],
+  items: [
+    // interface project
+    .file(
+      path: "\(DataPath)/\(name)DataInterface/Project.swift",
+      templatePath: "data_project_interface.stencil"
+    ),
+    // dto
+    .file(
+      path: "\(DataPath)/\(name)DataInterface/Sources/Entity/\(name)Entity.swift",
+      templatePath: "data_dto.stencil"
+    ),
+    // implement project
+    .file(
+      path: "\(DataPath)/\(name)Data/Project.swift",
+      templatePath: "data_project_implement.stencil"
+    ),
+    // repository
+    .file(
+      path: "\(DataPath)/\(name)Data/Sources/\(name)RepositoryImpl.swift",
+      templatePath: "data_repository.stencil"
+    ),
+    // test
+    .file(
+      path: "\(DataPath)/\(name)DataTesting/Sources/\(name)RepositoryTests.swift",
+      templatePath: "data_tests.stencil"
+    )
+  ]
+)

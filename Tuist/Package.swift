391be0ccea291cfee00586f27f0ee29f53d926f3
ir.__.si <cocoa9805@naver.com>
2025-06-05 23:41:05 +0900

    fix: SPM내부 dependency가 일부분 dynamic framework로 정의되어있던 부분 제거

--- Tuist/Package.swift
+++ Tuist/Package.swift
@@ -6,24 +6,7 @@ import PackageDescription
 #if TUIST
 import struct ProjectDescription.PackageSettings
 let packageSettings = PackageSettings(
-  productTypes: [
-    "ComposableArchitecture": .framework,
-    "Dependencies": .framework,
-    "Clocks": .framework,
-    "ConcurrencyExtras": .framework,
-    "CombineSchedulers": .framework,
-    "IdentifiedCollections": .framework,
-    "OrderedCollections": .framework,
-    "_CollectionsUtilities": .framework,
-    "DependenciesMacros": .framework,
-    "SwiftUINavigationCore": .framework,
-    "Perception": .framework,
-    "IssueReporting": .framework,
-    "CasePaths": .framework,
-    "CustomDump": .framework,
-    "XCTestDynamicOverlay": .framework,
-    "PerceptionCore": .framework
-  ]
+  productTypes: [:]
 )
 #endif
 

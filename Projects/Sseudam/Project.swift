1eb95553e9e4b3e1f548fe14254dcc582c4d7976
ji-yeon224 <69784492+ji-yeon224@users.noreply.github.com>
2025-06-05 15:16:30 +0900

    feat: #3 앱 진입 설정

---
+++ Projects/Sseudam/Project.swift
@@ -0,0 +1,29 @@
+//
+//  Project.swift
+//  Packages
+//
+//  Created by Jiyeon on 6/5/25.
+//
+
+import ProjectDescription
+import ProjectDescriptionHelpers
+
+let project = Project.makeApp(
+  infoPlist: [
+    "CFBundleDevelopmentRegion": "ko_KR",
+    "CFBundleShortVersionString": "1.0.0",
+    "CFBundleVersion": "1",
+    "CFBundleIconName": "AppIcon",
+    "UILaunchStoryboardName": "LaunchScreen",
+    "UIApplicationSceneManifest": [
+      "UIApplicationSupportsMultipleScenes": false,
+      "UISceneConfigurations": []
+    ],
+    "UISupportedInterfaceOrientations": [
+      "UIInterfaceOrientationPortrait"
+    ],
+  ],
+  dependencies: [
+    
+  ]
+)
---
+++ Projects/Sseudam/Sources/SseudamView.swift
@@ -0,0 +1,19 @@
+//
+//  SseudamView.swift
+//  Sseudam
+//
+//  Created by Jiyeon on 6/5/25.
+//  Copyright © 2025 Sseudam.a2bo.ios. All rights reserved.
+//
+
+import SwiftUI
+
+struct SseudamView: View {
+  var body: some View {
+    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  }
+}
+
+#Preview {
+  SseudamView()
+}

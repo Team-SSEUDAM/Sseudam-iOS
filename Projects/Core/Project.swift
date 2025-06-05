ee58ce0cd9b2b0e42c718aac90c865adeda91f80
ir.__.si <cocoa9805@naver.com>
2025-06-05 23:43:52 +0900

    fix: Core Project 구현
    - 기존에는 내부 Target으로 잡던 DesignSystem등도 모두 별도의 Project로 xcodeproj를 생성하였지만, 불필요하다고 판단하여 Source에 대한 target만 정의
    - 이후 ResourceKit도 Target으로 추가 될 예정

--- Projects/Core/Project.swift
+++ Projects/Core/Project.swift
@@ -8,11 +8,32 @@
 import ProjectDescription
 import ProjectDescriptionHelpers
 
-let project = Project.makeInternalLibraryProject(
-  for: InternalLibrary.Core,
-  dependencies: [
-    .Core.DesignKit
+let project = Project(
+  name: "Core",
+  organizationName: "Sseudam.a2bo.ios",
+  options: .default,
+  settings: .default,
+  targets: [
+    .target(
+      name: "DesignKit",
+      destinations: .iOS,
+      product: .staticLibrary,
+      bundleId: "Sseudam.a2bo.ios.Core",
+      deploymentTargets: .iOS("17.0"),
+      infoPlist: .extendingDefault(with: [:]),
+      sources: ["./DesignKit/Sources/**"]
+//      dependencies: [
+//        .Core.ResourceKit
+//      ]
+    )
+//    .target(
+//      name: "ResourceKit",
+//      destinations: .iOS,
+//      product: .bundle,
+//      bundleId: "Sseudam.a2bo.ios.ResourceKit",
+//      deploymentTargets: .iOS("17.0"),
+//      infoPlist: .extendingDefault(with: [:]),
+//      resources: ["./Resources/**"]
+//    )
   ]
 )
-
-

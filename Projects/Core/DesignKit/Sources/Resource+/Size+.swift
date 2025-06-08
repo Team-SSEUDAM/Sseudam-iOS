a85fa96d050e80c236c13f16683e94e178e3236c
ir.__.si <cocoa9805@naver.com>
2025-06-09 08:06:16 +0900

    add: #6 Size Asset 추가

---
+++ Projects/Core/DesignKit/Sources/Resource+/Size+.swift
@@ -0,0 +1,33 @@
+//
+//  Size+.swift
+//  DesignKit
+//
+//  Created by 조용인 on 3/16/25.
+//  Copyright © 2025 com.yongin.pida. All rights reserved.
+//
+
+import Foundation
+
+public extension CGFloat {
+  static let Number0: CGFloat = 0
+  static let Number2: CGFloat = 2
+  static let Number4: CGFloat = 4
+  static let Number6: CGFloat = 6
+  static let Number8: CGFloat = 8
+  static let Number10: CGFloat = 10
+  static let Number12: CGFloat = 12
+  static let Number14: CGFloat = 14
+  static let Number16: CGFloat = 16
+  static let Number18: CGFloat = 18
+  static let Number20: CGFloat = 20
+  static let Number24: CGFloat = 24
+  static let Number28: CGFloat = 28
+  static let Number32: CGFloat = 32
+  static let Number40: CGFloat = 40
+  static let Number48: CGFloat = 48
+  static let Number56: CGFloat = 56
+  static let Number64: CGFloat = 64
+  static let Number72: CGFloat = 72
+  static let Number80: CGFloat = 80
+  static let Number100: CGFloat = 100
+}

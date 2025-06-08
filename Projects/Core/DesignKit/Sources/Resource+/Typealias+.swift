585f537214c5bd8851fcbff905042791688d1085
ir.__.si <cocoa9805@naver.com>
2025-06-09 08:25:09 +0900

    add: #6 Icon Asset 추가

---
+++ Projects/Core/DesignKit/Sources/Resource+/Typealias+.swift
@@ -0,0 +1,13 @@
+//
+//  Typealias+.swift
+//  DesignKit
+//
+//  Created by 조용인 on 3/15/25.
+//  Copyright © 2025 com.yongin.pida. All rights reserved.
+//
+
+import Foundation
+
+public typealias ColorSet = DesignKitAsset.ColorSet
+public typealias FontSet = DesignKitFontFamily.FontSet
+public typealias Icons = DesignKitAsset.Icons

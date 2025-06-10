1cf58c94161e7319a754a9b3375cf99c9c732b4c
ji-yeon224 <69784492+ji-yeon224@users.noreply.github.com>
2025-06-10 16:41:58 +0900

    feat: #9 쓰레기통 데이터 entity 정의

--- Projects/Domain/Home/HomeDomainInterface/Sources/Entity/HomeEntity.swift
+++ Projects/Domain/Home/HomeDomainInterface/Sources/Entity/HomeEntity.swift
@@ -7,6 +7,32 @@
 //
 
 import Foundation
+import CoreLocation
 
+public struct TrashListEntity: Equatable, Sendable {
+  var list: [TrashItem]
+}
 
+public struct TrashItem: Equatable, Sendable {
+  public var id: Int
+  public var name: String
+  public var region: String
+  public var point: MapPoint
+  public var trashType: TrashType
+  
+}
 
+public enum TrashType: String, Sendable {
+  case recycle = "RECYCLE"
+  case general = "GENERAL"
+}
+
+public struct MapPoint: Equatable, Sendable {
+  public var latitude: Double
+  public var longitude: Double
+  
+  public init(latitude: Double, longitude: Double) {
+    self.latitude = latitude
+    self.longitude = longitude
+  }
+}

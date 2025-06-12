17031d0aeacc9e78e5c55ee52382c6d370ab6435
ir.__.si <cocoa9805@naver.com>
2025-06-12 17:17:29 +0900

    feat: #6 image asset 추가

--- Projects/Core/DesignKit/Sources/Resource+/Image+.swift
+++ Projects/Core/DesignKit/Sources/Resource+/Image+.swift
@@ -9,10 +9,13 @@
 import SwiftUI
 
 public enum ImageSet: String {
+  case propertyLeft = "Property=left"
   case add = "add"
   case addSpot = "addSpot"
   case arrowCircleUp = "arrow_circle_up"
   case cancel = "cancel"
+  case chevronRight = "chevron_right"
+  case close = "close"
   case delete = "delete"
   case deleteFill = "delete_fill"
   case edit = "edit"
@@ -21,9 +24,13 @@ public enum ImageSet: String {
   case info = "info"
   case interests = "interests"
   case lock = "lock"
+  case myLocation = "my_location"
   case person = "person"
   case personFilled = "person__filled"
   case recycle = "recycle"
+  case replay = "replay"
+  case search = "search"
+  case sentimentDissatisfied = "sentiment_dissatisfied"
   case settings = "settings"
   
   public var swiftUIImage: DesignKitImages { DesignKitImages(name: self.rawValue) }

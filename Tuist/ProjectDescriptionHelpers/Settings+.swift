eb695e2e2d3faaea6d7fd46b6b5a390fb5e1bbea
ir.__.si <cocoa9805@naver.com>
2025-06-18 17:56:09 +0900

    Feature/#18 fastlane match 인증서 관리 (#19)
    * feat: #18 fastlane init
    
    * feat: #18 fastlane match init
    
    * feat: #18 fastlane match default 작성
    
    * feat: #18 fastlane match 수정
    
    * feat: #18 xcode 오토사이닝 off & provisioning 및  code sign 세팅
    
    * fix: #18 tuist code sign identity 수정

--- Tuist/ProjectDescriptionHelpers/Settings+.swift
+++ Tuist/ProjectDescriptionHelpers/Settings+.swift
@@ -9,6 +9,32 @@ import Foundation
 import ProjectDescription
 
 extension Settings {
+  public static let release = Self.settings(
+    base: [
+      "DEVELOPMENT_TEAM": "6NXQDZ68FD",
+      "CODE_SIGN_STYLE": "Manual",
+      "PROVISIONING_PROFILE_SPECIFIER": "match Development Sseudam.a2bo.ios.Release",
+      "CODE_SIGN_IDENTITY": "Apple Development: yongin cho (B2J2829PJ5)"
+    ],
+    configurations: [
+      .debug(name: "Debug", xcconfig: .relativeToRoot("Config/Debug.xcconfig")),
+      .release(name: "Release", xcconfig: .relativeToRoot("Config/Release.xcconfig")),
+    ]
+  )
+  
+  public static let dev = Self.settings(
+    base: [
+      "DEVELOPMENT_TEAM": "6NXQDZ68FD",
+      "CODE_SIGN_STYLE": "Manual",
+      "PROVISIONING_PROFILE_SPECIFIER": "match Development Sseudam.a2bo.ios.Debug",
+      "CODE_SIGN_IDENTITY": "Apple Development: yongin cho (B2J2829PJ5)"
+    ],
+    configurations: [
+      .debug(name: "Debug", xcconfig: .relativeToRoot("Config/Debug.xcconfig")),
+      .release(name: "Release", xcconfig: .relativeToRoot("Config/Release.xcconfig")),
+    ]
+  )
+  
   public static let `default` = Self.settings(configurations: [
     .debug(name: "Debug", xcconfig: .relativeToRoot("Config/Debug.xcconfig")),
     .release(name: "Release", xcconfig: .relativeToRoot("Config/Release.xcconfig")),

092e21cb3b909b2f82e8ca9b378bdf9f03c07ce2
ir.__.si <cocoa9805@naver.com>
2025-06-22 14:13:49 +0900

    fix: #16 위치설정 View 구현

---
+++ Projects/Feature/Report/ReportFeature/Sources/Views/SubViews/MoveLocationView.swift
@@ -0,0 +1,53 @@
+//
+//  MoveLocationView.swift
+//  ReportFeature
+//
+//  Created by 조용인 on 6/20/25.
+//  Copyright © 2025 Sseudam.a2bo.ios. All rights reserved.
+//
+
+import SwiftUI
+import DesignKit
+import ComposableArchitecture
+
+public struct MoveLocationView: View {
+  @Bindable var store: StoreOf<MoveLocationFeature>
+  
+  public init(store: StoreOf<MoveLocationFeature>) {
+    self.store = store
+  }
+  
+  public var body: some View {
+    VStack(spacing: .Number24) {
+      VStack(alignment: .leading, spacing: .Number8) {
+        Text("지도를 움직여,\n쓰레기통 위치를 지정해주세요")
+          .font(FontSet.Heading.heading1)
+          .foregroundStyle(ColorSet.Text.Primary)
+        Text(store.address)
+          .font(FontSet.Body.body3)
+          .foregroundStyle(ColorSet.Text.Secondary)
+          .multilineTextAlignment(.leading)
+      }
+      ReportMapViewRepresentable(
+        userLocation: $store.userLocation
+      )
+      .onReceive {
+        store.send(.centerChanged($0))
+      }
+    }
+    .padding(.horizontal, .Number16)
+    .padding(.vertical, .Number24)
+
+  }
+}
+
+#Preview {
+  MoveLocationView(
+    store: Store(
+      initialState: MoveLocationFeature.State(
+        userLocation: .init(latitude: 37.5665, longitude: 126.9784)
+      ),
+      reducer: { MoveLocationFeature() }
+    )
+  )
+}

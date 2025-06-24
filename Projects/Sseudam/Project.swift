54dd6722c622d5cb1fed3a73fad2b7080173a285
ir.__.si <cocoa9805@naver.com>
2025-06-25 05:40:20 +0900

    feat: #16 struct 기반 CameraService 구현

--- Projects/Sseudam/Project.swift
+++ Projects/Sseudam/Project.swift
@@ -23,6 +23,7 @@ let project = Project.makeApp(
       "UIInterfaceOrientationPortrait"
     ],
     "NSLocationWhenInUseUsageDescription": "지도에서 내 위치를 확인하여 길찾기, 네비게이션 기능을 이용하기 위해 권한이 필요합니다.(필수권한)",
+    "NSCameraUsageDescription": "쓰레기통 사진을 촬영하여 업로드하기 위해 권한이 필요합니다.",
     "NMCLIENTID": "$(NM_CLIENT_ID)",
     "BASE_URL": "$(BASE_URL)"
   ],

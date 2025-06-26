dec55103cacd7ffaf5aec90c9943b8a149afe6ba
yongin9805 <jyi@vaultmicro.com>
2025-06-26 12:04:10 +0900

    feat: #16 갤러리 사진 선택 Feature 구현

---
+++ Projects/Feature/Report/ReportFeature/Sources/Features/PhotoLibraryPickerFeature.swift
@@ -0,0 +1,37 @@
+//
+//  PhotoLibraryPickerFeature.swift
+//  ReportFeature
+//
+//  Created by 조용인 on 6/26/25.
+//  Copyright © 2025 Sseudam.a2bo.ios. All rights reserved.
+//
+
+import SwiftUI
+import ComposableArchitecture
+
+@Reducer
+public struct PhotoLibraryPickerFeature {
+  @ObservableState
+  public struct State: Equatable {
+    public init() {}
+  }
+  
+  public enum Action: Equatable {
+    case delegate(Delegate)
+    
+    public enum Delegate: Equatable {
+      case photoSelected(UIImage)
+      case cancelled
+    }
+  }
+  
+  public var body: some ReducerOf<Self> {
+    Reduce { state, action in
+      switch action {
+      case .delegate:
+        return .none
+      }
+    }
+  }
+}
+

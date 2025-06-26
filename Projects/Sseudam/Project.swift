ef42430b89e0c043f09a487e60287b4dcd568fb6
yongin9805 <jyi@vaultmicro.com>
2025-06-26 10:55:36 +0900

    chore: #16 갤러리 접근 권한 info 작성

--- Projects/Sseudam/Project.swift
+++ Projects/Sseudam/Project.swift
@@ -24,6 +24,7 @@ let project = Project.makeApp(
     ],
     "NSLocationWhenInUseUsageDescription": "지도에서 내 위치를 확인하여 길찾기, 네비게이션 기능을 이용하기 위해 권한이 필요합니다.(필수권한)",
     "NSCameraUsageDescription": "쓰레기통 사진을 촬영하여 업로드하기 위해 권한이 필요합니다.",
+    "NSPhotoLibraryUsageDescription": "앱에서 사진을 선택하기 위해 갤러리 접근 권한이 필요합니다.",
     "NMCLIENTID": "$(NM_CLIENT_ID)",
     "BASE_URL": "$(BASE_URL)"
   ],

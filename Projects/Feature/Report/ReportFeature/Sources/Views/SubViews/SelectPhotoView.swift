1cbcaf1972d21bbcee5389cfe9518a772aa4070c
ir.__.si <cocoa9805@naver.com>
2025-06-26 16:49:02 +0900

    feat: #16 이미지 선택 시 View에 적용

--- Projects/Feature/Report/ReportFeature/Sources/Views/SubViews/SelectPhotoView.swift
+++ Projects/Feature/Report/ReportFeature/Sources/Views/SubViews/SelectPhotoView.swift
@@ -29,35 +29,55 @@ public struct SelectPhotoView: View {
             .font(FontSet.Body.body3)
             .foregroundColor(ColorSet.Text.Secondary)
         }
-        Rectangle()
-          .fill(ColorSet.Background.Secondary)
-          .aspectRatio(1, contentMode: .fit)
-          .clipShape(RoundedRectangle(cornerRadius: .Number16))
-          .overlay(
-            Icon(
-              image: .arrowCircleUp,
-              size: .Number64,
-              renderingMode: .template
-            )
-            .foregroundColor(ColorSet.Gray._200)
-          )
-          .onTapGesture {
-            store.send(.centerButtonTapped)
-          }
+        if store.selectedPhoto != nil {
+          selectedPhotoView
+        } else {
+          defaultPhotoView
+        }
       }
       .padding(.horizontal, .Number16)
       .padding(.vertical, .Number24)
       Spacer()
     }
-    .confirmationDialog($store.scope(state: \.destination?.confirmationDialog, action: \.destination.confirmationDialog))
-    .sheet(item: $store.scope(state: \.destination?.camera, action: \.destination.camera)) { store in
+    .fullScreenCover(item: $store.scope(state: \.destination?.camera, action: \.destination.camera)) { store in
       CameraPickerView(store: store)
     }
+    .confirmationDialog($store.scope(state: \.destination?.confirmationDialog, action: \.destination.confirmationDialog))
     .sheet(item: $store.scope(state: \.destination?.photoLibraryPicker, action: \.destination.photoLibraryPicker)) { store in
       PhotoLibraryPickerView(store: store)
     }
     .sheet(item: $store.scope(state: \.destination?.fileDocumentPicker, action: \.destination.fileDocumentPicker)) { store in
       FileDocumentPickerView(store: store)
     }
+  }
+  
+  @ViewBuilder
+  public var defaultPhotoView: some View {
+    Rectangle()
+      .fill(ColorSet.Background.Secondary)
+      .aspectRatio(1, contentMode: .fit)
+      .clipShape(RoundedRectangle(cornerRadius: .Number16))
+      .overlay(
+        Icon(
+          image: .arrowCircleUp,
+          size: .Number64,
+          renderingMode: .template
+        )
+        .foregroundColor(ColorSet.Gray._200)
+      )
+      .onTapGesture {
+        store.send(.centerButtonTapped)
+      }
+  }
+  
+  @ViewBuilder
+  public var selectedPhotoView: some View {
+    Image(uiImage: store.selectedPhoto ?? UIImage())
+      .resizable()
+      .aspectRatio(1, contentMode: .fit)
+      .clipShape(RoundedRectangle(cornerRadius: .Number16))
+      .onTapGesture {
+        store.send(.centerButtonTapped)
+      }
   }
 }

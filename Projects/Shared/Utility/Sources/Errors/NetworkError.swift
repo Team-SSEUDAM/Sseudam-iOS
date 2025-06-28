1a98c6bd166709abe6d4ec4401ccdf07146ea2ea
ir.__.si <cocoa9805@naver.com>
2025-06-28 18:21:34 +0900

    fix: NetworkError 타입 추가

--- Projects/Shared/Utility/Sources/Errors/NetworkError.swift
+++ Projects/Shared/Utility/Sources/Errors/NetworkError.swift
@@ -8,17 +8,21 @@
 
 import Foundation
 
-public enum NetworkError: Error, LocalizedError, Sendable {
+public enum NetworkError: Error, LocalizedError, Sendable, Equatable {
   
   case invalidStatusCode(Int)
   case timeout(TimeInterval)
   case serverError(message: String, code: Int)
+  case customError(message: String)
+  case taskCancelled
   
   public var errorDescription: String {
     switch self {
     case let .serverError(message, code): return "[에러코드: \(code)] - \(message)"
     case let .invalidStatusCode(code): return "[잘못 된 StatusCode] - \(code)"
     case let .timeout(time): return "[네트워크 요청 시간이 초과되었습니다.] - \(time)Seconds"
+    case let .customError(message): return "[커스텀 에러] - \(message)"
+    case .taskCancelled: return "[네트워크 요청이 취소되었습니다.]"
     }
   }
 }

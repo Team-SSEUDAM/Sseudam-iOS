bf4aebdf403c102d8d10c7ffeb1525de0d60dfa0
ir.__.si <cocoa9805@naver.com>
2025-07-16 18:24:39 +0900

    fix: #48 위치 정보 가져올 때, order 다양하게 가져와서 우선순위대로 불러오기

--- Projects/Feature/SelectSpotLocation/SelectSpotLocationFeature/Sources/Views/SelectSpotLocationView.swift
+++ Projects/Feature/SelectSpotLocation/SelectSpotLocationFeature/Sources/Views/SelectSpotLocationView.swift
@@ -19,40 +19,55 @@ public struct SelectSpotLocationView: View {
   }
   
   public var body: some View {
-    VStack(alignment: .leading, spacing: .Number24) {
-      VStack(alignment: .leading, spacing: .Number8) {
-        Text("지도를 움직여,\n쓰레기통 위치를 지정해주세요")
-          .font(FontSet.Heading.heading1)
-          .foregroundStyle(ColorSet.Text.Primary)
-        Text(store.address)
-          .font(FontSet.Body.body3)
-          .foregroundStyle(ColorSet.Text.Secondary)
-          .multilineTextAlignment(.leading)
-      }
-      SelectSpotMapViewRepresentable(
-        userLocation: $store.userLocation
-      )
-      .onReceive {
-        store.send(.centerChanged($0))
-      }
-      .onMapMovingStarted {
-        store.send(.onMapMovingStarted)
-      }
-      .overlay(
-        Icon(
-          image: .normalTrashPin,
-          size: .Number56
+    VStack {
+      VStack(alignment: .leading, spacing: .Number24) {
+        VStack(alignment: .leading, spacing: .Number8) {
+          Text("지도를 움직여,\n쓰레기통 위치를 지정해주세요")
+            .font(FontSet.Heading.heading1)
+            .foregroundStyle(ColorSet.Text.Primary)
+            .frame(maxWidth: .infinity, alignment: .leading)
+          Text(store.address)
+            .font(FontSet.Body.body3)
+            .foregroundStyle(ColorSet.Text.Secondary)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .lineLimit(1)
+        }
+        
+        Group {
+          SelectSpotMapViewRepresentable(
+            userLocation: $store.userLocation
+          )
+          .onReceive {
+            store.send(.centerChanged($0))
+          }
+          .onMapMovingStarted {
+            store.send(.onMapMovingStarted)
+          }
+          .overlay(
+            Icon(
+              image: .normalTrashPin,
+              size: .Number56
+            )
+            .offset(y: -16)
+            .allowsHitTesting(false),
+            alignment: .center
+          )
+        }
+        .aspectRatio(1, contentMode: .fit)
+        .clipCorners(.Number10, corners: .allCorners)
+        .overlay(
+          RoundedRectangle(cornerRadius: .Number10)
+            .stroke(ColorSet.Border.Secondary, lineWidth: .Number1)
         )
-        .offset(y: -16)
-        .allowsHitTesting(false),
-        alignment: .center
-      )
+        
+      }
+      .onAppear {
+        store.send(.onAppear)
+      }
+      .padding(.horizontal, .Number16)
+      .padding(.vertical, .Number24)
+      
+      Spacer()
     }
-    .onAppear {
-      store.send(.onAppear)
-    }
-    .padding(.horizontal, .Number16)
-    .padding(.vertical, .Number24)
-
   }
 }

e1e12167aa2ae0b6bc2904a76d791475a047c979
ji-yeon224 <69784492+ji-yeon224@users.noreply.github.com>
2025-09-02 09:43:06 +0900

    chore: 앱스토어 링크 constants로 변경

--- Projects/Shared/Utility/Sources/Type/ExternalURL.swift
+++ Projects/Shared/Utility/Sources/Type/ExternalURL.swift
@@ -12,5 +12,5 @@ public enum ExternalURL {
   public static let serviceTerm: URL? = URL(string: "https://wealthy-session-98c.notion.site/23f90c66759880bcbbabe5ba2f1b2650?source=copy_link")
   public static let privacyTerm: URL? = URL(string: "https://wealthy-session-98c.notion.site/23f90c66759880b4970ddb06f08e86a8?source=copy_link")
   public static let feedBack: URL? = URL(string: "https://walla.my/survey/BLp7yFuTUe7JI8KXPXvw")
-  public static let appStore: URL? = URL(string: "itms-apps://itunes.apple.com/app/id6751765360")
+  public static let appStore: URL? = URL(string: Constants.APP_STORE_URL)
 }
